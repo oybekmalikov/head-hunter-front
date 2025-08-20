ab5cb9693ebcc5fc586e5c6fdedf77df362ab930
Muhammad0712 <muhammadnematullayev7@gmail.com>
2025-08-20 19:45:01 +0500

    signIn signUp ResetPassword Otp pages UI done

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,63 @@
+"use client"
+import React from 'react'
+import '@ant-design/v5-patch-for-react-19';
+import { Button, Card, Checkbox, Form, Input } from 'antd';
+import Link from 'next/link';
+import { LockOutlined, MailOutlined } from '@ant-design/icons';
+
+const SignIn = () => {
+    return (
+        <div className='w-[100vw] h-[100vh] flex items-center justify-center'>
+            <Card
+                className='w-[450px] h-[480px] shadow-xl'
+            >
+                <h1 className='text-center text-3xl font-bold'>Welcome Back</h1>
+                <h2 className='text-center text-[15px] text-gray-500 mt-2'>Sign in to your account</h2>
+                <Form
+                    name="basic"
+                    layout='vertical'
+                    labelCol={{ span: 24 }}
+                    wrapperCol={{ span: 24 }}
+                    style={{ maxWidth: 450, maxHeight: 500, marginTop: 20 }}
+                    initialValues={{ remember: true }}
+                    autoComplete="on"
+                >
+                    <Form.Item label="Email Address" name="email" tooltip="This is a required field" htmlFor='email'>
+                        <Input
+                            prefix={<MailOutlined style={{ color: "#6B7280" }} />}
+                            className='flex gap-1'
+                            type='email'
+                            placeholder='Enter your email'
+                            size='large'
+                            id='email'
+                        />
+                    </Form.Item>
+                    <Form.Item label="Password:" name="password" tooltip="This is a required field" htmlFor='password'>
+                        <Input.Password
+                            prefix={<LockOutlined style={{ color: "#6B7280" }} />}
+                            className='flex gap-1'
+                            type='password'
+                            placeholder='Enter your password'
+                            size='large'
+                            id='password'
+                        />
+                    </Form.Item>
+                    <div className="w-[100%] h-[30px] flex justify-between items-center">
+                        <Checkbox>Remember me</Checkbox>
+                        <Link href={"/auth/forgot-password"}>Forgot Password?</Link>
+                    </div>
+                    <Form.Item>
+                        <Button type='primary' size='large' className='w-[100%] mt-6'>
+                            Sign In
+                        </Button>
+                    </Form.Item>
+                    <div className="">
+                        <p className='text-center'>Don`t have an account? <Link href="/auth/sign-up">Register</Link></p>
+                    </div>
+                </Form>
+            </Card>
+        </div>
+    )
+}
+
+export default SignIn
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+"use client"
 import '@ant-design/v5-patch-for-react-19';
+import { Button } from 'antd';
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      
-      <Link href="/users">Users</Link>
-    </>
+    <div className='flex gap-2'>
+      <Link href="/auth/sign-in" className='w-[100px] h-[40px] rounded-[8px] border border-blue-600 text-blue-600 flex items-center justify-center'>SignIn</Link>
+      <Button type='primary' size='large' className="w-[100px] h-[40px]" onClick={() => { window.location.href = "/auth/sign-up" }} >Sign Up</Button>
+    </div>
   );
 }
